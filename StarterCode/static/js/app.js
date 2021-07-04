a88f704ac5afe1354e9a09adf437dfb67713bf81
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-07-03 21:12:39 -0500

    arranging stuff

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,72 @@
+// Create a horizontal bar chart with a dropdown menu
+// to display the top 10 OTUs found in that individual.
+// Use sample_values as the values for the bar chart.
+// Use otu_ids as the labels for the bar chart.
+// Use otu_labels as the hovertext for the chart.
+
+
+// Setup frame
+const svgWidth = 960;
+const svgHeight = 500;
+const margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+
+// Create SVG wrapper
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+  // Append SVG group
+const chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Empty values
+var chartData = null;
+
+// Set up axes and initial parameters
+let chosenXAxis = "Poverty";
+let chosenYAxis = "Healthcare";
+
+var xAxisLabels = ["Poverty", "Age", "Income"];  // Default 
+var yAxisLabels = ["Obesity", "Smokes", "Healthcare"];
+var labelsTitle = { "poverty": "In Poverty (%)", 
+                    "age": "Age (Median)", 
+                    "income": "Household Income (Median)",
+                    "obesity": "Obese (%)", 
+                    "smokes": "Smokes (%)", 
+                    "healthcare": "Lacks Healthcare (%)" };
+
+function xScale(healthData,chosenXAxis){
+  var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(healthData, d=>d[chosenXAxis])*0.9, d3.max(healthData,d=>d[chosenXAxis])*1.1])
+      .range([0,width])
+  return xLinearScale;
+
+}
+
+function yScale(healthData, chosenYAxis) {
+    // Create Scales.
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(healthData, d => d[chosenYAxis]) * .9,d3.max(healthData, d => d[chosenYAxis]) * 1.1 ])
+        .range([height, 0]);
+  
+    return yLinearScale;
+  }
+
+// Update x-axis function
+function renderXAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+          .duration(1000)
+          .call(bottomAxis);
+  
+    return xAxis;
+  }
